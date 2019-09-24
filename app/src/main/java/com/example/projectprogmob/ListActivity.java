c26b78254281f29923015bbc57c30b9d3aa5aa5d
ryansahetapy <ryansahetapy@gmail.com>
2019-09-24 22:58:06 +0700

    Tugas7 terbaru

--- app/src/main/java/com/example/projectprogmob/ListActivity.java
+++ app/src/main/java/com/example/projectprogmob/ListActivity.java
@@ -2,6 +2,7 @@ package com.example.projectprogmob;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,60 +11,50 @@ import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class ListActivity extends AppCompatActivity {
+public class ListMahasiswaActivity extends AppCompatActivity {
+    String[] items ={"Harold", "Erick", "Aru", "Brian", "Ryan"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
-    }
-    String[] items={"lorem", "ipsum", "dolor", "sit", "amet",
-            "consectetuer", "adipiscing", "elit", "morbi", "vel",
-            "ligula", "vitae", "arcu", "aliquet", "mollis",
-            "etiam", "vel", "erat", "placerat", "ante",
-            "porttitor", "sodales", "pellentesque", "augue", "purus"};
+        setContentView(R.layout.activity_list_mahasiswa);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
+        ListView listView = findViewById(R.id.lvMhs);
+        Spinner spinner = findViewById(R.id.mhsSpinner);
 
-        ListView lv = findViewById(R.id.IvloremIpsun);
-        Spinner spinner = findViewById(R.id.spinner);
+        listView.setAdapter(new ArrayAdapter<String>(ListMahasiswaActivity.this, android.R.layout.simple_list_item_1,items));
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(ListMahasiswaActivity.this, android.R.layout.simple_spinner_item,items);
 
-        lv.setAdapter (new ArrayAdapter<string>(ListActivity.this, android.R.layout.simple_list_item_1,items));
-        ArrayAdapter<string> aa = new ArrayAdapter<>(ListActivity.this, android.R.layout.simple_spinner_item,items);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
 
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(aa);
-
-        Spinner.setAdapter(new ArrayAdapter<string>(ListActivity.this, android.R.layout.simple_spinner_item,items));
-
-        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(ListActivity.this, text:"anda memilih = " + items[1],Toast.LENGTH_LONG);
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(ListMahasiswaActivity.this, "Anda memilih = " + items [i], Toast.LENGTH_SHORT).show();
             }
 
             @Override
-            public  void onNothingSelected(AdapterView<?> adapterView){
-                Toast.makeText(ListActivity.this, text ="anda tidak memilih = ",Toast.LENGTH_SHORT).show();
-        }
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(ListMahasiswaActivity.this, "Anda tidak memilih", Toast.LENGTH_SHORT).show();
+            }
         });
 
-
-
-        lv.setAdapter(new ArrayAdapter<string>(ListActivity.this, android.R.layout.simple_list_item_1, items));
-
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int
-                    position, long id);{
-                        selection.setText(items[position]);
-
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(ListMahasiswaActivity.this, "Anda memilih = " +  items [i], Toast.LENGTH_SHORT).show();
             }
+        });
+    }
 
+    public void GoToCreate(View view) {
+        Intent intent = new Intent(ListMahasiswaActivity.this, CreateMhsActivity.class);
+        startActivity(Intent);
+    }
 
-
-        }
+    public void GoToUpdate(View view) {
+        Intent intent = new Intent(ListMahasiswaActivity.this, UpdateMhsActivity.class);
+        startActivity(Intent);
+    }
 }
